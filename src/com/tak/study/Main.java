a5e8e38a1831999cce9df7fc4461e362dd7bedd8
tak <wontaek22@gmail.com>
2023-04-03 22:14:10 +0900

    23.04.03
    백준 5622번 문제 (문자열 : 다이얼). 미완료

---
+++ src/com/tak/study/Main.java
@@ -0,0 +1,30 @@
+package com.tak.study;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //시간의 합구하기 -> cnt 선언 후 += 사용하여 합하기
+
+        Scanner sc = new Scanner(System.in);
+
+        //입력받을 문자
+        String str = sc.nextLine();
+
+        //횟수 담기
+        int cnt = 0;
+
+        //입력받은 문자의 길이만큼 반복
+        for (int i = 0; i < str.length(); i++) {
+
+            //오늘은 일하느라 지쳤으니.. 내일 이어서..
+
+        }
+
+
+
+    }
+
+}
